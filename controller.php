54412d4f1563e222a854369496b53964315da749
Stanislav Venkov <slavi104@gmaIl.com>
2015-04-27 23:38:11 +0300

    define global vars

--- controller.php
+++ controller.php
@@ -1,7 +1,22 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
 session_start();    //$root = "/newhungry/";
-$root = "/e_shop/";
+
+//var_dump($_SERVER['SERVER_NAME']);
+
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    define(DB, 'e_shop');
+    define(DB_USER, 'root');
+    define(DB_PSWD, '');
+    define(PATH_APP, "/e_shop/");
+} else {
+    define(DB, 'cashtrac_e_shop');
+    define(DB_USER, 'cashtrac');
+    define(DB_PSWD, 'txj)3vTA#W9G');
+    define(PATH_APP, "");
+}
+
+$root = PATH_APP;
 
 include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fException.php';
 include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fUnexpectedException.php';
@@ -9,7 +24,7 @@ include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fExpectedEx
 include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fActiveRecord.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fAuthorization.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fAuthorizationException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fBuffer.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fCRUD.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fCache.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fConnectivityException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fCookie.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fCore.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fCryptography.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fDatabase.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fDate.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fDirectory.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fEmail.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fEmptySetException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fEnvironmentException.php';  include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fFile.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fFilesystem.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fGrammar.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fHTML.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fImage.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fJSON.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fLoader.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fMailbox.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fMessaging.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fMoney.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fNoRemainingException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fNoRowsException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fNotFoundException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fNumber.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORM.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMColumn.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMDatabase.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMDate.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMFile.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMJSON.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMMoney.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMOrdering.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMRelated.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMSchema.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fORMValidation.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fPagination.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fProgrammerException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fRecordSet.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fRequest.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fResult.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fSMTP.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fSQLException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fSQLSchemaTranslation.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fSQLTranslation.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fSchema.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fSession.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fStatement.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fTemplating.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fText.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fTime.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fTimestamp.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fURL.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fUTF8.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fUnbufferedResult.php';  include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fUpload.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fValidation.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fValidationException.php'; include 'libs/flourishlib-flourish-classes-0.9.0-0-geb2d48b/flourish/fXML.php'; 
 
 //fORMDatabase::attach(new fDatabase('mysql', 'ihriulr4_games_for_brains', 'ihriulr4', 'xYY!5eu5s:FQ'));
-fORMDatabase::attach(new fDatabase('mysql', 'e_shop', 'root', ''));
+fORMDatabase::attach(new fDatabase('mysql', DB, DB_USER, DB_PSWD));
 
 
 class User extends fActiveRecord{
